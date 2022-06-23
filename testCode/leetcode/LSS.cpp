8e5f4ba072deec2801ad7d63ac52a89bd7629323
Jian <1367556195@qq.com>
2022-06-23 16:12:55 +0800

    新建一个最长和谐序列demo

---
+++ testCode/leetcode/LSS.cpp
@@ -0,0 +1,40 @@
+// 最长连续序列
+#include "../../../jian.h"
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> num_set;
+        for (const int& num : nums) {
+            num_set.insert(num);
+        }
+
+        int longestStreak = 0;
+
+        for (const int& num : num_set) {
+            if (!num_set.count(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.count(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;           
+    }
+};
+
+int main()
+{
+    
+    cout << "Hello" << endl;
+    int a = 10;
+    a = 11;
+    char c = getchar();
+
+    return 0;
+}
