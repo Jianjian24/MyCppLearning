a5e79132048c0cf3b7fdb9bcf30649eb7f0adfec
Jian <1367556195@qq.com>
2022-06-30 14:07:51 +0800

    复习Qt知识建立的初识笔记，包括一些基础知识和遇到的问题（qsrand、环境等）

---
+++ testCode/randTime.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void test_rand(void)
+    {
+    unsigned long n;
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < 100; i++)
+    {
+        n = rand();
+        printf("%d\n", n);
+    }
+}
+
+int main()
+{
+    test_rand();
+    return 0;
+}
