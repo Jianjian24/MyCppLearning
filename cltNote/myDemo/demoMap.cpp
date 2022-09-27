0cb7ff00d1319d9746165cbd6f40dd088f12146f
Jian <1367556195@qq.com>
2022-09-27 23:59:28 +0800

    update

---
+++ cltNote/myDemo/demoMap.cpp
@@ -0,0 +1,63 @@
+#include "iostream"
+#include "map"
+using namespace std;
+
+map<int, void*> myMap;
+
+typedef struct t1
+{
+    double a;
+    int b;
+}T1;
+
+typedef struct t2
+{
+    int a;
+    double b;
+}T2;
+
+void fun1()
+{
+    cout << "This is fun1()\n";
+    T1 * t = new T1;
+    t->a = 1;
+    pair<int, void*> p(1000, (void*)t);
+
+    myMap.insert(p);
+}
+
+void fun2()
+{
+    cout << "This is fun2()\n";
+    T2 * t = new T2;
+    t->a = 89;
+    pair<int, void*> p(2, (void*)t);
+
+    myMap.insert(p);
+}
+
+
+int main()
+{
+    fun1();
+    fun2();
+
+    int count = 0;  
+    for (auto iter = myMap.begin(); iter != myMap.end(); ++iter) 
+    {	
+        if (count == 0)
+        {
+            cout << iter->first << "  " << ((T1*)(iter->second))->a << endl;
+            ++count;
+            continue;
+        }
+        if (count == 1)
+        {
+            cout << iter->first << "  " << ((T1*)(iter->second))->b << endl;
+            ++count;
+        }			
+        
+    }
+
+    return 0;
+}
