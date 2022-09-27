351d5bb8455861573cfdc0d941f5b7aec9ec67ab
Jian <1367556195@qq.com>
2022-09-28 00:08:45 +0800

    update

--- cltNote/myDemo/demoMap.cpp
+++ cltNote/myDemo/demoMap.cpp
@@ -21,7 +21,8 @@ void fun1()
     cout << "This is fun1()\n";
     T1 * t = new T1;
     t->a = 1;
-    pair<int, void*> p(1000, (void*)t);
+    t->b = 1009;
+    pair<int, void*> p(1, (void*)t);
 
     myMap.insert(p);
 }
@@ -47,7 +48,7 @@ int main()
     {	
         if (count == 0)
         {
-            cout << iter->first << "  " << ((T1*)(iter->second))->a << endl;
+            cout << iter->first << "  " << ((T2*)(iter->second))->a << endl;
             ++count;
             continue;
         }
