f3fb590b4d20548dd820ef5058bfa0a9cc206186
Henry <1367556195@qq.com>
2023-07-17 12:49:20 +0800

    增加mfcPart1

---
+++ atHome/partTime/xyz/mfcShape/Shapes.7Dlg.h
@@ -0,0 +1,36 @@
+
+// Shapes.7Dlg.h : header file
+//
+
+#pragma once
+
+
+// CShapes7Dlg dialog
+class CShapes7Dlg : public CDialogEx
+{
+// Construction
+public:
+	CShapes7Dlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SHAPES7_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
